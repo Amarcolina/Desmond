53a381559118e902552dfef0b9f6c6bb63fbf38a
Alex <amarcoli@ucsc.edu>
2015-01-14 21:11:05 -0800

    Working on timeline

--- Assets/Desmond/Generated/DesmondSceneScript.cs
+++ Assets/Desmond/Generated/DesmondSceneScript.cs
@@ -1,15 +1,52 @@
 using UnityEngine;
-
+using System.IO;
+using System.Collections;
 
 public class DesmondSceneScript : DesmondSceneBase{
-    public UnityEngine.GameObject Cube;
-    public UnityEngine.GameObject Cube1;
-    public UnityEngine.GameObject MainCamera;
+    private System.Single timelineTime = 0.0f;
+    private System.Single timelineDirection = 1.0f;
+    private System.Single timelineDuration = 0.0f;
+    public AnimationCurve curve;
+    public UnityEngine.Transform Door;
     
-    public void Update(){
-        Debug.Log(Cube.transform.position.magnitude);
-        if(Input.GetKeyDown(UnityEngine.KeyCode.Space)){
-            Cube1.active = MainCamera.active;
+    private IEnumerator timelineCoroutine(){
+        bool finished = false;
+        while(!finished){
+            timelineTime += Time.deltaTime * timelineDirection;
+            if(timelineDirection == 1.0f){
+                if(timelineTime > timelineDuration){
+                    timelineTime = timelineDuration;
+                    finished = true;
+                }
+            }else{
+                if(timelineTime <= 0.0f){
+                    timelineTime = 0.0f;
+                    finished = true;
+                }
+            }
+            Door.localPosition = getCurve().Evaluate(timelineTime) * UnityEngine.Vector3.up;
+            yield return null;
         }
+    }
+    
+    public void Awake(){
+        timelineDuration = getCurve()[getCurve().length - 1].time;
+    }
+    
+    public void TimelinePlay(){
+        Debug.Log("here?");
+        timelineDirection = 1.0f;
+        StopCoroutine("timelineCoroutine");
+        StartCoroutine("timelineCoroutine");
+    }
+    
+    public void TimelinePlayReverse(){
+        timelineDirection = -1.0f;
+        StopCoroutine("timelineCoroutine");
+        StartCoroutine("timelineCoroutine");
+    }
+    
+    private UnityEngine.AnimationCurve getCurve(){
+        return curve;
     }
 }
