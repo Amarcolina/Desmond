12ef9bc493374f277f02286cfcfc2496ef6f2da2
Alex <amarcoli@ucsc.edu>
2015-01-21 23:12:04 -0800

    Updated the base script objects

---
+++ Assets/Desmond/Scripts/Base/DesmondEventBase.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DesmondEventBase : MonoBehaviour {
+    private DesmondSceneBase sceneScript;
+
+    public virtual void recieveEvent(DesmondEvent desmondEvent) {
+
+    }
+
+    protected void throwEvent(GameObject targetObject, DesmondEvent desmondEvent){
+        if (targetObject == null) {
+            if (sceneScript == null) {
+                sceneScript = FindObjectOfType<DesmondSceneBase>();
+            }
+            sceneScript.recieveEvent(desmondEvent);
+        } else {
+            DesmondEventBase[] eventBases = targetObject.GetComponents<DesmondEventBase>();
+            foreach (DesmondEventBase eventBase in eventBases) {
+                eventBase.recieveEvent(desmondEvent);
+            }
+        }
+    }
+}
--- Assets/Desmond/Scripts/Base/DesmondSceneBase.cs
+++ Assets/Desmond/Scripts/Base/DesmondSceneBase.cs
@@ -1,15 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DesmondSceneBase : MonoBehaviour {
+public class DesmondSceneBase : DesmondEventBase {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
