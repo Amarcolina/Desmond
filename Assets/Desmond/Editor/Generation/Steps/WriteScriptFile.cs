ebe464d286808fd4d153b90b57f8f5b55d9bb902
Alex <amarcoli@ucsc.edu>
2015-02-12 22:32:49 -0800

    Adding writescripttofile generation step

---
+++ Assets/Desmond/Editor/Generation/Steps/WriteScriptFile.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Desmond { 
+
+public class WriteScriptFile : GenerationStep {
+
+    public override void doStep() {
+
+        foreach (ScriptStruct script in scripts) {
+            List<string> lines = new List<string>();
+
+            foreach (string s in script.namespaceImports) {
+                lines.Add("using " + s + ";");
+            }
+
+            lines.Add("");
+            lines.Add("");
+
+            lines.Add("public class " + script.ToString() + " : DesmondSceneScript {");
+
+            foreach (FieldStruct field in script.fields.Values) {
+                lines.AddRange(field.generateScriptLines());
+            }
+
+            lines.Add("");
+
+            foreach (GenericMethodStruct method in script.methods.Values) {
+                lines.AddRange(method.generateScriptLines());
+                lines.Add("");
+            }
+
+            lines.Add("}");
+        }
+
+        string filePath = Application.dataPath + "/Desmond/Generated/SceneScripts/" + builder.scriptName + ".cs";
+        System.IO.File.WriteAllLines(filePath, lines);
+    }
+}
+
+}
