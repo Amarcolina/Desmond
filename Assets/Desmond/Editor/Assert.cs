62c86064b6199abf5a51ef1f615b66d35a8d59cd
Alex <amarcoli@ucsc.edu>
2015-03-04 20:41:56 -0800

    Adding assert class from other branch

---
+++ Assets/Desmond/Editor/Assert.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Desmond { 
+
+public class Assert {
+
+    public class AssertionFailed : System.Exception {
+        public AssertionFailed(string message) : base(message) { }
+    }
+
+    public static void that(bool condition, string message = "") {
+        if (!condition) {
+            throw new AssertionFailed(message);//
+        }
+    }
+
+    public static void equals(object a, object b, string message = "") {
+        if (!a.Equals(b)) {
+            throw new AssertionFailed(a + " did not equal " + b);
+        }
+    }
+}
+
+}
