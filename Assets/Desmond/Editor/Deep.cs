806f4564c723f134df9adc7cd91a2b6759e05c92
Alex <amarcoli@ucsc.edu>
2015-03-04 20:39:24 -0800

    Created Deep class

---
+++ Assets/Desmond/Editor/Deep.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IDeepObject {
+}
+
+public class Deep {
+    public static void copy(Object obj) {
+        
+    }
+
+    public static void destroy(Object obj) {
+
+    }
+}
